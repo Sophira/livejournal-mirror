f801d892100c3e06f7236478fa223ac23edac46d
janine <janine@1dfba125-df10-0410-8a77-af1a8d6d074f>
2007-06-25 17:05:36 +0000

    Journal titles widget, which uses AJAX to post its forms.
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/customize@11786 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Widget/JournalTitles.pm
@@ -0,0 +1,88 @@
+package LJ::Widget::JournalTitles;
+
+use strict;
+use base qw(LJ::Widget);
+use Carp qw(croak);
+
+sub ajax { 1 }
+sub need_res { qw( stc/widgets/journaltitles.css js/widgets/journaltitles.js ) }
+
+sub render_body {
+    my $class = shift;
+    my %opts = @_;
+
+    my $u = $opts{user} || LJ::get_remote();
+    $u = LJ::load_userid($u) unless LJ::isu($u);
+    die "Invalid user." unless LJ::isu($u);
+
+    my $ret;
+    $ret .= "<h2 class='widget-header'>" . $class->ml('widget.journaltitles.title') . "</h2>";
+    $ret .= "<p class='detail'>" . $class->ml('widget.journaltitles.desc') . "</p>";
+
+    foreach my $id (qw( journaltitle journalsubtitle friendspagetitle )) {
+        $ret .= $class->start_form( id => "${id}_form" );
+
+        $ret .= "<p>";
+        $ret .= "<label>" . $class->ml("widget.journaltitles.$id") . "</label> ";
+        $ret .= "<span id='${id}_view'>";
+        $ret .= "<strong>" . $u->prop($id) . "</strong> ";
+        $ret .= "<a class='theme-title-control' id='${id}_edit'>edit</a>";
+        $ret .= "</span>";
+
+        $ret .= "<span id='${id}_modify'>";
+        $ret .= $class->html_text(
+            name => 'title_value',
+            id => $id,
+            value => $u->prop($id),
+            size => '30',
+            maxlength => LJ::std_max_length(),
+            raw => "class='text'",
+        ) . " ";
+        $ret .= $class->html_hidden( which_title => $id );
+        $ret .= $class->html_hidden({ name => "user", value => $u->id, id => "${id}_user" });
+        $ret .= $class->html_submit("Save") . " ";
+        $ret .= "<a class='theme-title-control' id='${id}_cancel'>cancel</a>";
+        $ret .= "</span></p>";
+
+        $ret .= $class->end_form;
+    }
+
+    return $ret;
+}
+
+sub handle_post {
+    my $class = shift;
+    my $post = shift;
+    my %opts = @_;
+
+    my $u = $post->{user} || LJ::get_remote();
+    $u = LJ::load_userid($u) unless LJ::isu($u);
+    die "Invalid user." unless LJ::isu($u);
+
+    my $eff_val = LJ::text_trim($post->{title_value}, 0, LJ::std_max_length());
+    $eff_val = "" unless $eff_val;
+    $u->set_prop($post->{which_title}, $eff_val);
+}
+
+sub js {
+    q [
+        initWidget: function () {
+            var self = this;
+
+            DOM.addEventListener($("journaltitle_form"), "submit", function (e) { self.saveTitle(e, "journaltitle") });
+            DOM.addEventListener($("journalsubtitle_form"), "submit", function (e) { self.saveTitle(e, "journalsubtitle") });
+            DOM.addEventListener($("friendspagetitle_form"), "submit", function (e) { self.saveTitle(e, "friendspagetitle") });
+        },
+        saveTitle: function (e, id) {
+            this.userid = $(id + "_user").value;
+            this.doPostAndUpdateContent({which_title: id, title_value: $(id).value, user: this.userid});
+            Event.stop(e); // prevent the page from posting without AJAX
+        },
+        onRefresh: function (data) {
+            this.initWidget();
+            JournalTitle.init();
+        },
+    ];    
+}
+
+1;
