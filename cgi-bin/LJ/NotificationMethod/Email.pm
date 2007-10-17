247d1aa95fc50c9eb441c7879e2e9fde43624ae6
ahassan <ahassan@1dfba125-df10-0410-8a77-af1a8d6d074f>
2007-10-17 17:36:06 +0000

    Merge Whitaker's changes
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/r16.1@12827 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/NotificationMethod/Email.pm
+++ cgi-bin/LJ/NotificationMethod/Email.pm
@@ -3,7 +3,9 @@ package LJ::NotificationMethod::Email;
 use strict;
 use Carp qw/ croak /;
 use base 'LJ::NotificationMethod';
-require "$ENV{LJHOME}/cgi-bin/weblib.pl";
+
+use lib "$ENV{LJHOME}/cgi-bin";
+require "weblib.pl";
 
 sub can_digest { 1 };
 
@@ -65,7 +67,7 @@ sub notify {
         croak "invalid event passed" unless ref $ev;
 
         my $footer = "\n\n-- \n$LJ::SITENAME Team\n$LJ::SITEROOT";
-        $footer .= LJ::run_hook("esn_email_footer");
+        $footer .= LJ::run_hook("esn_email_footer", $ev, $u);
         $footer .= "\n\nIf you prefer not to get these updates, you can change your preferences at $LJ::SITEROOT/manage/subscriptions/";
 
         my $plain_body = LJ::run_hook("esn_email_plaintext", $ev, $u);
