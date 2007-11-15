d8537b58fdd1e49e1715d8d695324491295bc8be
janine <janine@1dfba125-df10-0410-8a77-af1a8d6d074f>
2007-11-15 00:17:22 +0000

    Bugfix: LJ::Entry->new() needs a user object, not a userid.
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/vertical-content@13053 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Vertical.pm
+++ cgi-bin/LJ/Vertical.pm
@@ -533,7 +533,8 @@ sub first_entry {
 sub entry_singletons {
     my $self = shift;
 
-    return map { LJ::Entry->new($_->[0], jitemid => $_->[1]) } @_;
+    my $users = LJ::load_userids(map { $_->[0] } @_);
+    return map { LJ::Entry->new($users->{$_->[0]}, jitemid => $_->[1]) } @_;
 }
 
 sub children {
