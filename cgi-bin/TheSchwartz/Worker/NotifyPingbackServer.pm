4a79df721b260aa6bfe6bddca13ad1a1d26c3750
vad <vad@1dfba125-df10-0410-8a77-af1a8d6d074f>
2008-12-16 10:26:06 +0000

    Remove debug output from cgi-bin/TheSchwartz/Worker/NotifyPingbackServer.pm Revert changes in htdocs/stc/fck/editor/dialog/fck_link/fck_link.js
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/pingback@14672 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/TheSchwartz/Worker/NotifyPingbackServer.pm
+++ cgi-bin/TheSchwartz/Worker/NotifyPingbackServer.pm
@@ -10,7 +10,6 @@ sub work {
     my ($class, $job) = @_;
     my $args = $job->arg;
     my $client = $job->handle->client;
-warn "start send ping...";
     my $res = eval {
         send_ping(uri  => $args->{uri},
                   text => $args->{text},
@@ -27,25 +26,16 @@ sub send_ping {
     my $uri  = $args{uri};
     my $text = $args{text};
     my $mode = $args{mode};
-warn "   mode: $mode";
-warn "   uri:  $uri";
-    # TODO: move pingback's uri to config
+    
     my $pb_server_uri = $LJ::PINGBACK->{uri};
-#                            . ($LJ::PINGBACK->{uri} =~ m|/$| ? '' : '/')
-#                            . 'ljping/';
-warn "   pb server: $pb_server_uri";
     my $content = JSON::objToJson({ uri => $uri, text => $text, mode => $mode }) . "\r\n";
 
     my $headers = HTTP::Headers->new;
        $headers->header('Content-Length' => length $content);
 
     my $req = HTTP::Request->new('POST', $pb_server_uri, $headers, $content );
-
-warn "REQ: " . $req->as_string;
-
     my $ua  = LWP::UserAgent->new;
     my $res = $ua->request($req);
-warn "   response: " . $res->content;
 
     return 1 if $res->content eq 'OK';
     return 0;
