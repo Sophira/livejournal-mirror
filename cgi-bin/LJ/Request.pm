56807cba0879bc248b67cbc44c750701995b8653
tupshin <tupshin@1dfba125-df10-0410-8a77-af1a8d6d074f>
2008-12-28 06:32:52 +0000

    Adding Request abstraction layer from dreamwidth to modernize branch
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/modernize@14729 1dfba125-df10-0410-8a77-af1a8d6d074f

---
+++ cgi-bin/LJ/Request.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+#
+# LJ::Request
+#
+# This module provides an abstraction layer for accessing data traditionally
+# available through Apache::Request and similar modules.
+#
+# Authors:
+#      Mark Smith <mark@dreamwidth.org>
+#
+# Copyright (c) 2008 by Dreamwidth Studios, LLC.
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself.  For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+#
+
+package LJ::Request;
+
+use strict;
+use LJ::Request::Apache2;
+
+use vars qw( $cur_req $determined );
+
+# creates a new LJ::Request object, based on what type of server environment we
+# are running under
+sub get {
+    # if we have already run this logic, return it.  makes it safe for us in case
+    # the logic below is a little heavy so it doesn't run over and over.
+    return $cur_req if $determined;
+
+    # attempt Apache 2
+    eval {
+        eval "use Apache2::RequestUtil ();";
+        my $r = Apache2::RequestUtil->request;
+        $cur_req = LJ::Request::Apache2->new( $r )
+            if $r;
+    };
+
+    # hopefully one of the above worked and set $cur_req, but if not, then we
+    # assume we're in fallback/command line mode
+    $determined = 1;
+    return $cur_req;
+}
+
+# called after we've finished up a request, or before a new request, as long as
+# it's called sometime it doesn't matter exactly when it happens
+sub reset {
+    $determined = 0;
+    $cur_req = undef;
+}
+
+1;
---
+++ cgi-bin/LJ/Request/Apache2.pm
@@ -0,0 +1,163 @@
+#!/usr/bin/perl
+#
+# LJ::Request::Apache2
+#
+# Abstraction layer for Apache 2/mod_perl 2.
+#
+# Authors:
+#      Mark Smith <mark@dreamwidth.org>
+#
+# Copyright (c) 2008 by Dreamwidth Studios, LLC.
+#
+# This program is free software; you may redistribute it and/or modify it under
+# the same terms as Perl itself.  For a copy of the license, please reference
+# 'perldoc perlartistic' or 'perldoc perlgpl'.
+#
+
+package LJ::Request::Apache2;
+
+use strict;
+use Apache2::Const -compile => qw/ :common REDIRECT HTTP_NOT_MODIFIED /;
+use Apache2::Log ();
+use Apache2::Request;
+use Apache2::Response ();
+use Apache2::RequestRec ();
+use Apache2::RequestUtil ();
+use Apache2::RequestIO ();
+
+use fields (
+            'r',         # The Apache2::Request object
+            'post_args', # hashref of POST arguments
+        );
+
+# creates a new LJ::Request object, based on what type of server environment we
+# are running under
+sub new {
+    my LJ::Request::Apache2 $self = $_[0];
+    $self = fields::new( $self ) unless ref $self;
+
+    # setup object
+    $self->{r}         = $_[1];
+    $self->{post_args} = undef;
+
+    # done
+    return $self;
+}
+
+# current document root
+sub document_root {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->document_root;
+}
+
+# method string GET, POST, etc
+sub method {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->method;
+}
+
+# the URI requested (does not include host:port info)
+sub uri {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->uri;
+}
+
+sub content_type {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->content_type($_[1]);
+}
+
+# returns the query string
+sub query_string {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->args;
+}
+
+# get POST arguments as an APR::Table object (which is a tied hashref)
+sub post_args {
+    my LJ::Request::Apache2 $self = $_[0];
+    unless ( $self->{post_args} ) {
+        my $tmp_r = Apache2::Request->new( $self->{r} );
+        $self->{post_args} = $tmp_r->body;
+    }
+    return $self->{post_args};
+}
+
+# searches for a given note and returns the value, or sets it
+sub note {
+    my LJ::Request::Apache2 $self = $_[0];
+    if ( scalar( @_ ) == 2 ) {
+        return $self->{r}->notes->{$_[1]};
+    } else {
+        return $self->{r}->notes->{$_[1]} = $_[2];
+    }
+}
+
+# searches for a given header and returns the value, or sets it
+sub header_in {
+    my LJ::Request::Apache2 $self = $_[0];
+    if ( scalar( @_ ) == 2 ) {
+        return $self->{r}->headers_in->{$_[1]};
+    } else {
+        return $self->{r}->headers_in->{$_[1]} = $_[2];
+    }
+}
+
+# searches for a given header and returns the value, or sets it
+sub header_out {
+    my LJ::Request::Apache2 $self = $_[0];
+    if ( scalar( @_ ) == 2 ) {
+        return $self->{r}->headers_out->{$_[1]};
+    } else {
+        return $self->{r}->headers_out->{$_[1]} = $_[2];
+    }
+}
+
+# returns the ip address of the connected person
+sub get_remote_ip {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->connection->remote_ip;
+}
+
+# sets last modified
+sub set_last_modified {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->set_last_modified($_[1]);
+}
+
+sub status_line {
+    my LJ::Request::Apache2 $self = $_[0];
+    if (scalar(@_) == 2) {
+        # Apparently both status and status_line must be set
+        my ($status) = $_[1] =~ m/^(\d+)/;
+        $self->{r}->status($status);
+        return $self->{r}->status_line($_[1]);
+    } else {
+        return $self->{r}->status_line();
+    }
+}
+
+# meets conditions
+sub meets_conditions {
+    my LJ::Request::Apache2 $self = $_[0];
+    return $self->{r}->meets_conditions;
+}
+
+sub print {
+    my LJ::Request::Apache2 $self = shift;
+    return $self->{r}->print(@_);
+}
+
+sub read {
+    my LJ::Request::Apache2 $self = shift;
+    my $ret = $self->{r}->read(@_);
+    return $ret;
+}
+
+# constants
+sub OK {
+    my LJ::Request::Apache2 $self = $_[0];
+    return Apache2::Const::OK;
+}
+
+1;
