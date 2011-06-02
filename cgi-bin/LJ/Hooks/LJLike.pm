0abb1c16706a2817b6842684c4fb6ebeae19768f
gariev <gariev@1dfba125-df10-0410-8a77-af1a8d6d074f>
2011-06-02 11:31:39 +0000

    LJSUP-8952: <lj like> tag Moving Google JS from head to body for IE
    git-svn-id: http://code.livejournal.org/svn/livejournal/branches/r79.4@19206 1dfba125-df10-0410-8a77-af1a8d6d074f

--- cgi-bin/LJ/Hooks/LJLike.pm
+++ cgi-bin/LJ/Hooks/LJLike.pm
@@ -2,6 +2,7 @@ package LJ::Hooks::LJLike;
 use strict;
 use warnings;
 
+=comment
 LJ::register_hook( 'sitewide_resources' => sub {
     return if $LJ::REQ_GLOBAL{'sitewide_resources_ljlike_google'}++;
 
@@ -12,6 +13,7 @@ LJ::register_hook( 'sitewide_resources' => sub {
 
     LJ::include_raw( 'html' => qq{<script type="text/javascript" src="http://apis.google.com/js/plusone.js">{lang: $locale}</script>} );
 } );
+=cut
 
 LJ::register_hook( 'insert_html_after_body_open' => sub {
     my ($after_body_open_ref) = @_;
@@ -20,6 +22,12 @@ LJ::register_hook( 'insert_html_after_body_open' => sub {
     my $locale = LJ::lang_to_locale($language);
 
     $$after_body_open_ref .= qq{<div id="fb-root"></div><script src="http://connect.facebook.net/$locale/all.js#appId=214181831945836&amp;xfbml=1"></script>};
+    if (!$LJ::REQ_GLOBAL{'sitewide_resources_ljlike_google'}++) {
+        my $language = LJ::Lang::get_remote_lang();
+        my $locale = LJ::lang_to_locale($language);
+        $locale =~ s/_.*//g;
+        $$after_body_open_ref .=  qq{<script type="text/javascript" src="http://apis.google.com/js/plusone.js">{lang: $locale}</script>};
+    }
 } );
 
 LJ::register_hook( 'sitewide_resources' => sub {
